18e6d22b9eb75690974438045bd39020f5a551cb
Andrew Robinson <andrew.robinson@gmail.com>
2012-11-28 14:32:38 -0500

    Added some of the controls from the reddit app.

---
+++ Fusao.Controls/CueTextBox.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace Fusao.Controls
+{
+    public sealed class CueTextBox : TextBox 
+    {
+        private enum TextBoxState { Empty, Typing, HasText };
+        private TextBoxState _currentState = TextBoxState.Empty;
+
+        private TextBox _mainBox;
+
+        public CueTextBox()
+        {
+            this.DefaultStyleKey = typeof(CueTextBox);
+            this.Text = "";
+            this.ActualTextBrush = this.CueTextBrush; 
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            _mainBox = (TextBox)GetTemplateChild("MainTextBox");
+            _mainBox.GotFocus += CueTextBox_GotFocus;
+            _mainBox.LostFocus += CueTextBox_LostFocus;
+            _mainBox.TextChanged += OnTextChanged;
+            _mainBox.Text = this.CueText;
+            base.OnApplyTemplate();
+        }
+
+        private void CueTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (_currentState == TextBoxState.Empty)
+            {
+                _mainBox.Text = "";
+                this.ActualTextBrush = this.Foreground;
+            }
+            _currentState = TextBoxState.Typing;
+        }
+
+        private void CueTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(_mainBox.Text))
+            {
+                _currentState = TextBoxState.Empty;
+                _mainBox.Text = this.CueText;
+                this.ActualTextBrush = this.CueTextBrush;
+            }
+            else
+            {
+                _currentState = TextBoxState.HasText;
+            }
+        }
+
+        public static DependencyProperty CueTextProperty = DependencyProperty.Register("CueText", typeof(string), typeof(CueTextBox), new PropertyMetadata("", OnCueTextChanged));
+
+        private static void OnCueTextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            CueTextBox c = (CueTextBox)sender;
+            c.OnCueTextChanged(e);
+        }
+
+        private void OnCueTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (_mainBox != null && _currentState == TextBoxState.Empty)
+            {
+                _mainBox.Text = this.CueText;
+                this.ActualTextBrush = this.CueTextBrush;                
+            }
+        }
+
+        public string CueText
+        {
+            get
+            {
+                return (string)GetValue(CueTextProperty);
+            }
+            set
+            {
+                SetValue(CueTextProperty, value);
+            }
+        }
+
+        private void OnTextChanged(object sender, RoutedEventArgs e)
+        {
+            if (_mainBox != null && _currentState == TextBoxState.Typing)
+            {
+                this.Text = _mainBox.Text;
+            }
+        }
+
+        public static DependencyProperty ActualTextBrushProperty = DependencyProperty.Register("ActualTextBrush", typeof(Brush), typeof(CueTextBox), new PropertyMetadata(null));
+
+        public Brush ActualTextBrush
+        {
+            get
+            {
+                return (Brush)GetValue(ActualTextBrushProperty);
+            }
+
+            set
+            {
+                SetValue(ActualTextBrushProperty, value);
+            }
+        }
+
+        public static DependencyProperty CueTextBrushProperty = DependencyProperty.Register("CueTextBrush", typeof(Brush), typeof(CueTextBox), new PropertyMetadata(new SolidColorBrush(Windows.UI.Colors.Gray)));
+
+        public Brush CueTextBrush
+        {
+            get
+            {
+                return (Brush)GetValue(CueTextBrushProperty);
+            }
+
+            set
+            {
+                SetValue(CueTextBrushProperty, value);
+            }
+        }
+    }
+}
---
+++ Fusao.Controls/RotateButton.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Fusao.Controls
+{
+    [TemplatePart(Name = "MainButton", Type = typeof(Button))]
+    public sealed class RotateButton : Control
+    {
+        public static DependencyProperty CommandProperty = DependencyProperty.Register(
+           "Command",
+           typeof(ICommand),
+           typeof(RotateButton),
+           new PropertyMetadata(null)
+       );
+
+        public ICommand Command
+        {
+            get
+            {
+                return (ICommand)GetValue(CommandProperty);
+            }
+            set
+            {
+                SetValue(CommandProperty, value);
+            }
+        }
+
+        public static DependencyProperty IsRotatedProperty = DependencyProperty.Register(
+            "IsRotated",
+            typeof(bool),
+            typeof(RotateButton),
+            new PropertyMetadata(false, IsRotatedChanged)
+        );
+
+        private static void IsRotatedChanged(Object sender, DependencyPropertyChangedEventArgs e)
+        {
+            RotateButton instance = (RotateButton)sender;
+            instance.IsRotatedChanged(e);
+        }
+
+        private void IsRotatedChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (_mainButton == null)
+            {
+                return;
+            }
+
+            if (e.OldValue == e.NewValue)
+            {
+                return;
+            }
+
+            double from = 0;
+            double to = 0;
+
+            if ((bool)e.OldValue)
+            {
+                to = 180;
+            }
+            else
+            {
+                from = 180;
+            }
+
+            DoubleAnimation d = new DoubleAnimation()
+            {
+               BeginTime = new TimeSpan(0),
+               Duration = new Duration(TimeSpan.FromMilliseconds(450)),
+               From = from,
+               To = to,
+               FillBehavior = FillBehavior.HoldEnd
+            };
+
+            Storyboard s = new Storyboard();
+            s.Children.Add(d);
+            Storyboard.SetTarget(d, _mainButton);
+            Storyboard.SetTargetProperty(d, "(UIElement.RenderTransform).(RotateTransform.Angle)");
+            s.Begin();
+        }
+
+        public bool IsRotated
+        {
+            get
+            {
+                return (bool)GetValue(IsRotatedProperty);
+            }
+            set
+            {
+                SetValue(IsRotatedProperty, value);
+            }
+        }
+
+        private Button _mainButton = null;
+        public RotateButton()
+        {
+            this.DefaultStyleKey = typeof(RotateButton);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _mainButton = (Button)GetTemplateChild("MainButton");
+        }
+    }
+}
