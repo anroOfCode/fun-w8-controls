f78fae4ba180b9c383d2e983e5dcdf1dacef582b
Andrew Robinson <andrew.robinson@gmail.com>
2012-12-02 19:55:00 -0500

    Rotate button now supports vertical orientation

--- Fusao.Controls/RotateButton.cs
+++ Fusao.Controls/RotateButton.cs
@@ -15,12 +15,13 @@ namespace Fusao.Controls
     [TemplatePart(Name = "MainButton", Type = typeof(Button))]
     public sealed class RotateButton : Control
     {
+        #region Command DependencyProperty
         public static DependencyProperty CommandProperty = DependencyProperty.Register(
            "Command",
            typeof(ICommand),
            typeof(RotateButton),
            new PropertyMetadata(null)
-       );
+        );
 
         public ICommand Command
         {
@@ -33,7 +34,9 @@ namespace Fusao.Controls
                 SetValue(CommandProperty, value);
             }
         }
+        #endregion
 
+        #region IsRotated DependencyProperty
         public static DependencyProperty IsRotatedProperty = DependencyProperty.Register(
             "IsRotated",
             typeof(bool),
@@ -49,42 +52,12 @@ namespace Fusao.Controls
 
         private void IsRotatedChanged(DependencyPropertyChangedEventArgs e)
         {
-            if (_mainButton == null)
-            {
-                return;
-            }
-
             if (e.OldValue == e.NewValue)
             {
                 return;
             }
 
-            double from = 0;
-            double to = 0;
-
-            if ((bool)e.OldValue)
-            {
-                to = 180;
-            }
-            else
-            {
-                from = 180;
-            }
-
-            DoubleAnimation d = new DoubleAnimation()
-            {
-               BeginTime = new TimeSpan(0),
-               Duration = new Duration(TimeSpan.FromMilliseconds(450)),
-               From = from,
-               To = to,
-               FillBehavior = FillBehavior.HoldEnd
-            };
-
-            Storyboard s = new Storyboard();
-            s.Children.Add(d);
-            Storyboard.SetTarget(d, _mainButton);
-            Storyboard.SetTargetProperty(d, "(UIElement.RenderTransform).(RotateTransform.Angle)");
-            s.Begin();
+            PerformRotationChange();
         }
 
         public bool IsRotated
@@ -98,8 +71,41 @@ namespace Fusao.Controls
                 SetValue(IsRotatedProperty, value);
             }
         }
+        #endregion
+
+        #region IsVertical DependencyProperty
+        public static DependencyProperty IsVerticalProperty = DependencyProperty.Register(
+            "IsVertical",
+            typeof(bool),
+            typeof(RotateButton),
+            new PropertyMetadata(false, IsVerticalChanged)
+        );
+
+        private static void IsVerticalChanged(Object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ((RotateButton)sender).IsVerticalChanged(e);
+        }
+
+        private void IsVerticalChanged(DependencyPropertyChangedEventArgs e)
+        {
+            PerformOrientationChange();     
+        }
+
+        public bool IsVertical
+        {
+            get
+            {
+                return (bool)GetValue(IsVerticalProperty);
+            }
+            set
+            {
+                SetValue(IsVerticalProperty, value);
+            }
+        }
+        #endregion
 
         private Button _mainButton = null;
+
         public RotateButton()
         {
             this.DefaultStyleKey = typeof(RotateButton);
@@ -109,6 +115,109 @@ namespace Fusao.Controls
         {
             base.OnApplyTemplate();
             _mainButton = (Button)GetTemplateChild("MainButton");
+            PerformOrientationChange();
+        }
+
+        private void PerformOrientationChange()
+        {
+            if (_mainButton == null)
+            {
+                return;
+            }
+
+            RotateTransform rTransform = _mainButton.RenderTransform as RotateTransform;
+            if (rTransform != null)
+            {
+                rTransform.Angle = LookupToAngle();
+            }
+        }
+
+        private void PerformRotationChange()
+        {
+            if (_mainButton == null)
+            {
+                return;
+            }
+
+            double from = LookupFromAngle();
+            double to = LookupToAngle();
+
+            DoubleAnimation d = new DoubleAnimation()
+            {
+                BeginTime = new TimeSpan(0),
+                Duration = new Duration(TimeSpan.FromMilliseconds(450)),
+                From = from,
+                To = to,
+                FillBehavior = FillBehavior.HoldEnd 
+            };
+
+            Storyboard s = new Storyboard();
+            s.Children.Add(d);
+            Storyboard.SetTarget(d, _mainButton);
+            Storyboard.SetTargetProperty(d, "(UIElement.RenderTransform).(RotateTransform.Angle)");
+            s.Begin();
+
+            d.Completed += delegate
+            {
+                RotateTransform rTransform = _mainButton.RenderTransform as RotateTransform;
+                if (rTransform != null)
+                {
+                    rTransform.Angle = to;
+                }
+                s.Stop();
+            };           
+        }
+
+        private double LookupToAngle()
+        {
+            if (this.IsRotated)
+            {
+                if (this.IsVertical)
+                {
+                    return 90;
+                }
+                else
+                {
+                    return 180;
+                }
+            }
+            else
+            {
+                if (this.IsVertical)
+                {
+                    return 270;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        private double LookupFromAngle()
+        {
+            if (this.IsRotated)
+            {
+                if (this.IsVertical)
+                {
+                    return 270;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                if (this.IsVertical)
+                {
+                    return 90;
+                }
+                else
+                {
+                    return 180;
+                }
+            }
         }
     }
 }
